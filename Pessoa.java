69717f4a5c619745488d688e8b25d06e2136e69a
Felipe Paz Carvalho Batista <lipepaz23@gmail.com>
2025-09-01 15:58:07 -0300

    Pessoa (POO) - done

---
+++ Pessoa.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+// Classe Pessoa:
+// Atributos privados: nome e idade
+// Construtor: inicializa atributos
+// Apresentação(): Imprime atributos do objeto
+public class Pessoa {
+
+    // Declara atributos
+    private String nome;
+    private int idade;
+
+    // Inicializa atributos
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void apresentar() {
+        System.out.println("Ola, meu nome e " + nome + " e tenho " + idade + " anos.");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("PESSOA " + (i + 1) + ":");
+
+            // Armazena nome do objeto
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+
+            // Armazena idade do objeto
+            System.out.print("Idade: ");
+            int idade = sc.nextInt();
+            sc.nextLine(); // Consome ENTER que sobrou
+
+            // Inicializa objeto com nome e idade
+            pessoas.add(new Pessoa(nome, idade));
+        }
+
+        sc.close();
+
+        // Imprime pessoas registradas
+        for (Pessoa p : pessoas) {
+            p.apresentar();
+        }
+    }
+}
